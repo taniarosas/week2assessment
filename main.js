d34427f379e4ee39c21b755bba1c32b2acea74a9
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-29 10:56:08 -0700

    week 2 assessment

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+	alert("hey you clicked me!");
+	});
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+	});
+});
